503ef3259033a3e6ee9db0e0f200c121ec041ff7
aldyivans <ivanbrutalart@Gmail.com>
2024-09-21 13:05:12 +0700

    currency

---
+++ currency/currency.js
@@ -0,0 +1,19 @@
+const currency = Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 });
+const number = Intl.NumberFormat('id-ID', { minimumFractionDigits: 0 });
+
+export const toIDR = (v, symbol = true) => {
+  let modV = v;
+
+  if (!isNaN(v)) {
+    const stringArr = v?.toString().split('.');
+    if (stringArr?.length > 1) {
+      modV = symbol ? currency.format(v) : `${number.format(+stringArr[0])},${stringArr[1]}`;
+    } else {
+      modV = symbol ? currency.format(v) : v;
+    }
+  }
+
+  return modV
+}
+
+export default currency;
